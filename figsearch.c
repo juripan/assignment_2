4c37c9d50dffa523a4d88c63312e7a6ebcc345cd
Juraj Panoc <juraj.panoc@gmail.com>
2024-11-23 14:41:02 +0100

    added find_horizontal_line function, refactored some functions, rewrote the test functions so that they work with the given specification

--- figsearch.c
+++ figsearch.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 
 #define MAX_LINE_LEN 101
 #define print_err(msg) fprintf(stderr, msg "\n")
@@ -29,23 +31,26 @@ int arg_parse(int count, char** args);
 
 void print_help();
 
+int test_command(char* file_name);
+
 int test_file_structure(char* file_name);
-
-int find_vertical_line(char* file_name, Coords* start, Coords* end);
-
-int find_horizontal_line();
-
-int find_square();
 
 int get_size(FILE* file, unsigned* rows, unsigned* columns);
 
 int validate_bitmap(FILE* file, unsigned width, unsigned height);
 
+int find_line(char* file_name, int find_func(Image*, Coords*, Coords*));
+
+int find_vertical_line(Image* img, Coords* start, Coords* end);
+
+int find_horizontal_line(Image* img, Coords* start, Coords* end);
+
+int find_square();
+
 
 int main(int argc, char** argv){
-
-    int err = arg_parse(argc - 1, argv);
-    if(err) return EXIT_FAILURE;
+    if(arg_parse(argc - 1, argv) == EXIT_FAILURE)
+        return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
 }
@@ -65,6 +70,41 @@ Image* image_ctor(unsigned height, unsigned width){
     return img;
 }
 
+Image* load_image_data(char* file_name){
+    /*
+    creates an Image struct on the stack,
+    loads file bitmap image into the Image struct from the file,
+    returns a pointer to the Image struct
+    */
+    FILE* file = fopen(file_name, "r");
+    if(file == NULL){
+        print_err("fopen failed");
+        return NULL;
+    }
+    unsigned col, row;
+
+    //checking the return is redundant since it was checked in test_file_structure
+    get_size(file, &col, &row);
+
+    Image* img = image_ctor(col, row);
+
+    if(img == NULL){
+        print_err("Image allocation failed");
+        fclose(file);
+        return NULL;
+    }
+
+    char buff;
+    unsigned data_index = 0;
+
+    while((buff = fgetc(file)) != EOF){
+        if(buff == '1' || buff == '0'){
+            img->data[data_index++] = buff - '0'; // subtracting '0' converts the char into an int
+        }
+    }
+    fclose(file);
+    return img;
+}
 
 int* get_bit(Image* img, unsigned row, unsigned col){
     if(row > img->height || col > img->width){
@@ -82,46 +122,32 @@ void image_dtor(Image* img){
 
 
 int arg_parse(int count, char** args){
+    /*
+    parses the passed in arguments and triggers the corresponding function,
+    returns EXIT_FAILURE if anything goes wrong, otherwise retruns EXIT_SUCCESS 
+    */
     if(count > 2){
         print_err("Too many of arguments");
         return EXIT_FAILURE;
     }
     if(count == 1 && strcmp(args[1], "--help") == 0){
         print_help();
+        return EXIT_SUCCESS;
     }
     else if(count == 2 && strcmp(args[1], "test") == 0){
-        if(test_file_structure(args[2]) == EXIT_FAILURE)
-            return EXIT_FAILURE;
-        printf("Valid\n");
+        return test_command(args[2]);
     }
-    else if(count == 2 && strcmp(args[1], "hline") == 0){
-        //Coords start = {0};
-        //Coords end = {0};
-        //if(test_file_structure(args[2]) == EXIT_FAILURE 
-        //|| find_horizontal_line(args[2], &start, &end) == EXIT_FAILURE){
-        //    return EXIT_FAILURE;
-        //}
-        //printf("start row %d col %d\n", start.row, start.col);
-        //printf("end row %d col %d\n", end.row, end.col);
+    if(count == 2 && strcmp(args[1], "hline") == 0){
+        return find_line(args[2], find_horizontal_line);
     }
-    else if(count == 2 && strcmp(args[1], "vline") == 0){
-        Coords start = {0};
-        Coords end = {0};
-        if(test_file_structure(args[2]) == EXIT_FAILURE 
-        || find_vertical_line(args[2], &start, &end) == EXIT_FAILURE){
-            return EXIT_FAILURE;
-        }
-        printf("start row %d col %d\n", start.row, start.col);
-        printf("end row %d col %d\n", end.row, end.col);
+    if(count == 2 && strcmp(args[1], "vline") == 0){
+        return find_line(args[2], find_vertical_line);
     }
-    else if(count == 2 && strcmp(args[1], "square") == 0){
+    if(count == 2 && strcmp(args[1], "square") == 0){
         //find_square();
     }
-    else{
-        print_err("Incorrect arguments");
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
+    print_err("Incorrect arguments");
+    return EXIT_FAILURE;
 }
 
 
@@ -144,10 +170,19 @@ void print_help(){
 }
 
 
-int test_file_structure(char* file_name){ //TODO: make this fit the requirements
+int test_command(char* file_name){
+    if(test_file_structure(file_name) == EXIT_FAILURE){
+        return EXIT_FAILURE;   
+    }
+    printf("Valid\n");
+    return EXIT_SUCCESS;
+}
+
+
+int test_file_structure(char* file_name){
     /*
     opens a file with the file name that is passed in and checks if it has a valid file structure,
-    returns EXIT_FAILURE if the test fails, else it returns EXIT_SUCCESS
+    returns EXIT_FAILURE if the test fails of the file doesn't open, else it returns EXIT_SUCCESS
     */
     FILE* file = fopen(file_name, "r");
     unsigned height, width;
@@ -167,10 +202,94 @@ int test_file_structure(char* file_name){ //TODO: make this fit the requirements
 }
 
 
-int find_vertical_line(char* file_name, Coords* start, Coords* end){
+int get_size(FILE* file, unsigned* rows, unsigned* columns){
+    /*
+    reads and validates the first 2 numbers (size of bitmap),
+    writes into the rows and columns addresses the result of parsing the size,
+    returns EXIT_FAILURE if it fails,
+    */
+    char buff[MAX_LINE_LEN];
+    char* next; // points at the expected next char
+    char* end; // points at the end of the string (end of the line)
+    int whitespace_count = 0;
+    unsigned i;
+
+    for(i = 0; (buff[i] = fgetc(file)) != EOF; i++){
+        if(isspace(buff[i])!= 0){
+            whitespace_count++;
+            if(whitespace_count == 2){ //reads the first two numbers
+                break;
+            }
+        }
+    }
+    buff[i+1] = '\0';
+
+    *rows = strtol(buff, &next, 10);
+    
+    *columns = strtol(next, &end, 10);
+
+    if(*rows == 0 || *columns == 0){
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int validate_bitmap(FILE* file, unsigned width, unsigned height){
+    /*
+    reads the bitmap part of the file and checks if the dimensions fit
+    and if the format of the file is correct,
+    returns EXIT_FAILURE if the validation failed,
+    */
+    char buff;
+    unsigned bit_count = 0;
+
+    for(unsigned i = 0; (buff = fgetc(file)) != EOF; i++){
+        if(/*i % 2 == 0 &&*/ (buff == '1' || buff == '0')){ //UNCOMMENT THESE WHEN TESTING ON LINUX
+            bit_count++;
+        }
+        else if(/*i % 2 != 0 &&*/ isspace(buff)){
+            continue;
+        }
+        else{
+            return EXIT_FAILURE;
+        }
+    }
+
+    if(bit_count != width * height){
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int find_line(char* file_name, int find_func(Image*, Coords*, Coords*)){
+    /*
+    function that finds the longest line (orientation is specified by the function passed in),
+    prints the coordinates of the start and the end of the line found,
+    returns EXIT_FAILURE if any of the functions fail
+    */
+    Coords start = {0};
+    Coords end = {0};
+
+    if(test_file_structure(file_name) == EXIT_FAILURE){
+        return EXIT_FAILURE;
+    }
+
     Image* img = load_image_data(file_name);
     if(img == NULL) return EXIT_FAILURE;
 
+    if(find_func(img, &start, &end) == EXIT_FAILURE){
+        return EXIT_FAILURE;
+    }
+
+    printf("start row %d col %d\n", start.row, start.col);
+    printf("end row %d col %d\n", end.row, end.col);
+    return EXIT_SUCCESS;
+}
+
+
+int find_vertical_line(Image* img, Coords* start, Coords* end){ 
     unsigned max_score = 0;
     unsigned score = 0;
 
@@ -196,104 +315,26 @@ int find_vertical_line(char* file_name, Coords* start, Coords* end){
 }
 
 
-int get_size(FILE* file, unsigned* rows, unsigned* columns){
-    /*
-    reads and validates the first line (size of bitmap),
-    writes into the rows and columns addresses the result of parsing the size,
-    returns EXIT_FAILURE if it fails,
-    */
-    char buff[MAX_LINE_LEN];
-    char* next; // points at the expected next char
-    char* end; // points at the end of the string (end of the line)
-    
-    if(fgets(buff, sizeof(buff), file) == NULL){ 
-        return EXIT_FAILURE;
-    }
+int find_horizontal_line(Image* img, Coords* start, Coords* end){
+    unsigned max_score = 0;
+    unsigned score = 0;
 
-    *rows = strtol(buff, &next, 10);
-
-    if(*next != ' '){
-        return EXIT_FAILURE;
-    }
-    next++;
-    
-    *columns = strtol(next, &end, 10);
-
-    if(*rows == 0 || *columns == 0 || *end != '\r'){ //CHANGE THIS WHEN TESTING ON LINUX TO \n
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-}
-
-
-int validate_bitmap(FILE* file, unsigned width, unsigned height){
-    /*
-    reads the bitmap part of the file and checks if the dimensions fit
-    and if the format of the file is correct,
-    returns EXIT_FAILURE if the validation failed,
-    */
-    char buffer[MAX_LINE_LEN]; //TODO: maybe make it a malloc so the buffer is adjusted to its width
-    unsigned row_count = 0, bit_count = 0;
-
-    while(fgets(buffer, sizeof(buffer), file) != NULL){
-        for(unsigned i = 0; buffer[i] != '\r' && buffer[i] != '\0'; i++){ //CHANGE THIS WHEN TESTING ON LINUX TO \n
-            if(i % 2 == 0 && (buffer[i] == '1' || buffer[i] == '0')){
-                bit_count++;
-            }
-            else if(i % 2 != 0 && buffer[i] == ' '){
-                continue;
-            }
-            else{
-                return EXIT_FAILURE;
+    for(unsigned r = 0; r < img->height; r++){
+        for(unsigned c = 0; c < img->width; c++){ // reads rows
+            if(*get_bit(img, r, c) == 1){
+                score++;
+                if(score > max_score){
+                    start->row = r; // start has to be in the same row as the end since this is a horizontal line
+                    start->col = c - score + 1; // score is used as an offset for the first occurrence of 1
+                    end->row = r;
+                    end->col = c;
+                    max_score = score;
+                }
+            } else {
+                score = 0; // breaks the line
             }
         }
-        row_count++;
-    }
-
-    if(row_count != height || bit_count != width * height){
-        return EXIT_FAILURE;
+        score = 0; //resets the score for a new column
     }
     return EXIT_SUCCESS;
-}
-
-
-Image* load_image_data(char* file_name){
-    /*
-    creates an Image struct on the stack,
-    loads file bitmap image into the Image struct from the file,
-    returns a pointer to the Image struct
-    */
-    FILE* file = fopen(file_name, "r");
-    if(file == NULL){
-        print_err("fopen failed");
-        return NULL;
-    }
-    unsigned col, row;
-
-    if(get_size(file, &col, &row) == EXIT_FAILURE){ //TODO: remove the check if redundant
-        print_err("Invalid data");
-        fclose(file);
-        return NULL;
-    }
-
-    Image* img = image_ctor(col, row);
-
-    if(img == NULL){
-        print_err("Image allocation failed");
-        fclose(file);
-        return NULL;
-    }
-
-    char buffer[MAX_LINE_LEN];
-    unsigned data_index = 0;
-
-    while(fgets(buffer, sizeof(buffer), file) != NULL){
-        for(unsigned i = 0; buffer[i] != '\r' && buffer[i] != '\0'; i++){ //CHANGE THIS WHEN TESTING ON LINUX TO \n
-            if(i % 2 == 0 && (buffer[i] == '1' || buffer[i] == '0')){
-                img->data[data_index++] = buffer[i] - '0'; // subtracting '0' converts the char into an int
-            }
-        }
-    }
-    fclose(file);
-    return img;
 }
